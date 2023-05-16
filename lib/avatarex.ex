6048dc6a360db16f07ece5b3c1f7810060e36dcb
David Kuhta <davidkuhta@gmail.com>
2023-05-15 22:54:01 -0400

    Initial commit

---
+++ lib/avatarex.ex
@@ -0,0 +1,18 @@
+defmodule Avatarex do
+  @moduledoc """
+  Documentation for `Avatarex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Avatarex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/avatarex_test.exs
@@ -0,0 +1,8 @@
+defmodule AvatarexTest do
+  use ExUnit.Case
+  doctest Avatarex
+
+  test "greets the world" do
+    assert Avatarex.hello() == :world
+  end
+end
